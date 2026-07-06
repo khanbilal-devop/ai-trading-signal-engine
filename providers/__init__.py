0513c5a8b4fa9f81dfae3804419a31a2b722168e
Ahmed Khan <khanbial13@gmail.com>
2026-07-06 17:52:42 -0400

    Base file of the providers folder

--- providers/__init__.py
+++ providers/__init__.py
@@ -1,3 +1,13 @@
 from .base import NewsProvider
+from .marketaux import MarketauxProvider
+from .alphavantage import AlphaVantageProvider
+from .finnhub import FinnhubProvider
+from .aggregator import NewsAggregator
 
-__all__ = ["NewsProvider"]
+__all__ = [
+    "NewsProvider",
+    "MarketauxProvider",
+    "AlphaVantageProvider",
+    "FinnhubProvider",
+    "NewsAggregator",
+]
